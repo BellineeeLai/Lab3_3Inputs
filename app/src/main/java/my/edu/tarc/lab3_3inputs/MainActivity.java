2eeb4b527bdbecce1cca6025d15eeb514738d163
BellineeeLai <laicy-wa16@student.tarc.edu.my>
2018-12-18 13:05:03 +0800

    Updated

--- app/src/main/java/my/edu/tarc/lab3_3inputs/MainActivity.java
+++ app/src/main/java/my/edu/tarc/lab3_3inputs/MainActivity.java
@@ -13,87 +13,139 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-    private Spinner spinnerAge;
-    private RadioGroup radioGroupGender;
-    private RadioButton radioButtonMale, radioButtonFemale;
-    private CheckBox checkBoxSmoker;
-    private TextView textViewPremium;
+
+    private Spinner ageSpinner;
+    private RadioGroup genderGroup ;
+    private RadioButton maleButton;
+    private RadioButton femaleButton;
+    private CheckBox smokerBox;
+    private TextView amountText;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        spinnerAge = findViewById(R.id.spinnerAge);
-        radioGroupGender = findViewById(R.id.radioGroup);
-        radioButtonMale = findViewById(R.id.radioButtonMale);
-        radioButtonFemale = findViewById(R.id.radioButtonFemale);
-        checkBoxSmoker = findViewById(R.id.checkBoxSmoker);
-        textViewPremium = findViewById(R.id.textViewPremium);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.age_group,android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerAge.setAdapter(adapter);
-        spinnerAge.setOnItemSelectedListener(this);
+
+        genderGroup = findViewById(R.id.radioGroupGender);
+        ageSpinner = findViewById(R.id.age_spinner);
+        maleButton = findViewById(R.id.maleRadio);
+        femaleButton = findViewById(R.id.femaleRadio);
+        smokerBox = findViewById(R.id.smokerBox);
+        amountText = findViewById(R.id.premiumField);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.age_group,android.R.layout.simple_dropdown_item_1line);
+        ageSpinner.setAdapter(adapter);
+        ageSpinner.setOnItemSelectedListener( this);
+
+    }
+
+    public void calculateInsurance(View view)
+    {
+        int premium = 0;
+        int position ;
+        String gender;
+        boolean isSmoker = smokerBox.isChecked();
+        position = ageSpinner.getSelectedItemPosition();
+
+        if(genderGroup.getCheckedRadioButtonId() == R.id.maleRadio)
+        {
+            gender = "male";
+        }
+        else
+        {
+            gender = "female";
+        }
+
+        switch(position){
+            case 0 :
+                premium = 50;
+                break;
+            case 1 :
+                premium = 55;
+                break;
+            case 2 :
+                premium = 60;
+                if(gender == "male")
+                {
+                    premium += 50;
+                }
+                break;
+            case 3 :
+                premium = 70;
+                if(gender == "male")
+                {
+                    premium += 100;
+                    if(isSmoker)
+                    {
+                        premium +=100;
+                    }
+                }
+                break;
+            case 4 :
+                premium = 120;
+                if(gender == "male")
+                {
+                    premium += 100;
+                    if(isSmoker)
+                    {
+                        premium +=150;
+                    }
+                }
+                break;
+            case 5 :
+                premium = 160;
+                if(gender == "male")
+                {
+                    premium += 50;
+                    if(isSmoker)
+                    {
+                        premium +=150;
+                    }
+                }
+                break;
+            case 6 :
+                premium = 200;
+                if(gender == "male")
+                {
+                    premium += 0;
+                    if(isSmoker)
+                    {
+                        premium +=250;
+                    }
+                }
+                break;
+            case 7 :
+                premium = 250;
+                if(gender == "male")
+                {
+                    premium += 0;
+                    if(isSmoker)
+                    {
+                        premium +=250;
+                    }
+                }
+                break;
+        }
+
+        amountText.setText("RM" + premium);
+
+    }
+
+    public void resetEverything(View view){
+        amountText.setText("");
+        genderGroup.clearCheck();
+        smokerBox.setChecked(false);
+        ageSpinner.setSelection(0);
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        Toast.makeText(this,"Position="+position,Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"Item Selected "+ position,Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
-    }
-
-    private void calculatePremium(View view){
-        float premium=0;
-        int ageGroup;
-        ageGroup = spinnerAge.getSelectedItemPosition();
-        if(ageGroup ==0){
-            premium = 50;
-        }else if(ageGroup == 1){
-            premium = 55;
-        }else if(ageGroup == 2){
-            premium = 60;
-        }else if(ageGroup == 3){
-            premium = 70;
-        }else if(ageGroup == 4){
-            premium = 90;
-        }else if(ageGroup == 5){
-            premium = 120;
-        }else if(ageGroup == 6){
-            premium = 150;
-        }else if(ageGroup ==7){
-            premium = 150;
-        }
-
-        int gender = radioGroupGender.getCheckedRadioButtonId();
-        if(gender == R.id.radioButtonMale){
-            if(ageGroup == 3){
-                premium += 50;
-            }else if(ageGroup == 4){
-                premium += 100;
-            }else if(ageGroup == 5){
-                premium += 150;
-            }else if(ageGroup == 6){
-                premium += 200;
-            }else if(ageGroup == 7){
-                premium += 200;
-            }
-        }
-
-        if(checkBoxSmoker.isChecked()){
-            if(ageGroup == 3){
-                premium += 100;
-            }else if(ageGroup == 4){
-                premium += 150;
-            }else if(ageGroup == 5){
-                premium += 200;
-            }else if(ageGroup == 6){
-                premium += 250;
-            }else if(ageGroup == 7){
-                premium += 300;
-            }
-        }
-        textViewPremium.setText(getString(R.string.premium) + " = " +premium);
     }
 }
